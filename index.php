b0c02503b49f9e4b2598b737f0948930807462fb
javi <ingenierodesoftware73@gmail.com>
2023-04-01 21:31:28 -0500

    controlador frontal

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
+<h1>Página con mvc</h1>
 <?php
     require_once 'controllers/Usuarios.php';
-    $controlador = new UsuarioController();
-    $controlador->showAll();
+    if(isset($_GET['controller']) && class_exists($_GET['controller'])){
+        $controlador = $_GET['controller'];
+        $controlador = new $controlador();
 
-    $controlador->create();
+        if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
+            $action = $_GET['action'];
+            $controlador->$action();
+    
+        }else{
+            echo "No existe la página que buscas";
+        }
+    }else{
+            echo "No existe la página que buscas";
+    }
+    
 ?>
